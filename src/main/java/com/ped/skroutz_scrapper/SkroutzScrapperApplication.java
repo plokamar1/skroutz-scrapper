46ba2fe748c21ff1299b5d123fde071bf6881695
Panagiotis Dorgiakis <ped@empist.com>
2024-08-18 20:28:46 +0300

    Initialize

---
+++ src/main/java/com/ped/skroutz_scrapper/SkroutzScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.ped.skroutz_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkroutzScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkroutzScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ped/skroutz_scrapper/SkroutzScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ped.skroutz_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkroutzScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
