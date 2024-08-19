efc66d565b6d008b17c801bcb3aa34c5901dcd2b
Panagiotis Dorgiakis <ped@empist.com>
2024-08-19 18:46:12 +0300

    Adding and removing categories from a product

--- src/main/java/com/ped/skroutz_scrapper/models/Product.java
+++ src/main/java/com/ped/skroutz_scrapper/models/Product.java
@@ -2,6 +2,7 @@ package com.ped.skroutz_scrapper.models;
 
 import java.net.URL;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import jakarta.persistence.*;
@@ -34,4 +35,19 @@ public class Product {
     @ManyToMany
     @JoinTable(name = "category_product", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    public boolean addCategory(Category _category) {
+        return categories.add(_category);
+    }
+
+    public boolean removeCategory(long _categoryId) {
+        for (Iterator<Category> it = categories.iterator(); it.hasNext();) {
+            Category c = it.next();
+            if (c.getId() == _categoryId) {
+                categories.remove(c);
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/com/ped/skroutz_scrapper/models/ProductTest.java
+++ src/test/java/com/ped/skroutz_scrapper/models/ProductTest.java
@@ -15,6 +15,7 @@ public class ProductTest {
 
     Product product;
     Category category;
+    Set<Category> categories = new HashSet<>();
 
     @BeforeEach
     public void setup() throws MalformedURLException {
@@ -24,11 +25,8 @@ public class ProductTest {
                 .link(categoryUrl)
                 .build();
 
-        Set<Category> categories = new HashSet<>();
         categories.add(category);
-
         URL productUrl = new URL("https://localhost.com/product");
-
         product = Product.builder()
                 .title("Huawei Mobile Phone")
                 .price(256.45)
@@ -130,12 +128,58 @@ public class ProductTest {
     }
 
     @Test
-    void testSetCategories() {
+    void testSetCategories() throws MalformedURLException {
+        URL url = new URL("https://test.com");
+        Category newCategory = Category.builder()
+                .link(url)
+                .title("Another Test")
+                .build();
 
+        categories.add(newCategory);
+        product.setCategories(categories);
+
+        assertThat(product.getCategories().size()).isEqualTo(2);
+        assertThat(product.getCategories().toArray()[1]).isEqualTo(newCategory);
     }
 
     @Test
-    void testSetLink() {
+    void testSetLink() throws MalformedURLException {
+        URL newUrl = new URL("https://test.com/test");
+        product.setLink(newUrl);
 
+        assertThat(product.getLink()).isInstanceOf(URL.class);
+        assertThat(product.getLink().toString()).isEqualTo(newUrl.toString());
+    }
+
+    @Test
+    void testAddCategory() throws MalformedURLException {
+        URL url = new URL("https://test.com");
+
+        Category newCategory = Category.builder()
+                .link(url)
+                .title("Another Test")
+                .build();
+
+        assertThat(product.addCategory(newCategory)).isTrue();
+        assertThat(product.getCategories().size()).isEqualTo(2);
+    }
+
+    @Test
+    void testShouldNotAddCategory() {
+        assertThat(product.addCategory(category)).isFalse();
+        assertThat(product.getCategories().size()).isEqualTo(1);
+    }
+
+    @Test
+    void testRemoveCategory() {
+        assertThat(product.removeCategory(category.getId())).isTrue();
+        assertThat(product.getCategories().size()).isEqualTo(0);
+    }
+
+    @Test
+    void testShouldNotRemoveCategory() {
+        long fakeId = 1231232;
+        assertThat(product.removeCategory(fakeId)).isFalse();
+        assertThat(product.getCategories().size()).isEqualTo(1);
     }
 }
